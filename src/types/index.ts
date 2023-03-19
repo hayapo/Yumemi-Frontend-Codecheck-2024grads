4e9238df2c749fc7766f3e3198202ab4b7505298
hayapo <keep.forward1001@gmail.com>
2023-03-20 02:54:24 +0900

    [feature] 人口構成情報用の方を定義

--- src/types/index.ts
+++ src/types/index.ts
@@ -14,3 +14,26 @@ export type PrefectureResponse = {
 export type PrefecturesWithChecked = Prefecture & {
   checked: boolean
 }
+
+/* RESAS APIから帰ってくるレスポンスの中の、人口構成情報の型 */
+export type PopulationPerYear = {
+  year: number
+  value: number
+}
+
+/* 都道府県ごとの人口構成情報を扱う型 */
+export type PrefecturePopulation = Prefecture & {
+  populations: PopulationPerYear[]
+}
+
+/* 都道府県別人口構成取得(API/api/v1/population)のレスポンスの型 */
+export type PopulationResponse = {
+  message: string | null
+  result: {
+    boundaryYear: number
+    data: {
+      label: string
+      data: PopulationPerYear[]
+    }[]
+  }
+}
