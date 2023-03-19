cb042ce59d8cffdf59be31ee449b2dde81a254a8
hayapo <keep.forward1001@gmail.com>
2023-03-20 03:07:22 +0900

    [feature] useGetPopulationsフックを使用して 都道府県ごとの人口データが含まれたリストを返すカスタムフック

---
+++ src/features/PopulationChart/lib/hooks/usePopulations.ts
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react"
+import { PrefecturePopulation } from "@/types"
+import { useGetPopulations } from "../api"
+import { useCheckedPrefecture } from "../hooks"
+
+/**
+ * useGetPopulationsフックを使用して\
+ * 都道府県ごとの人口データが含まれたリストを返す
+ * @function
+ * @returns {PrefecturePopulation[]} 都道府県ごとの人口情報が含まれた配列
+ */
+export const usePopulations = () => {
+  const [prefecuturePopulations, setPrefecturePopulations] = useState<PrefecturePopulation[]>([])
+  const checkedPrefectures = useCheckedPrefecture()
+  const getPopulations = useGetPopulations()
+  let ingnore = false
+
+  useEffect(() => {
+    const fetchPopulation = async () => {
+      try {
+        const data = await Promise.all(
+          checkedPrefectures.map(async (pref) => {
+            const populations = await getPopulations(pref.prefCode)
+            return { ...pref, populations: populations }
+          })
+        )
+        setPrefecturePopulations(data)
+      } catch (error: unknown) {
+        if (error instanceof Error) console.error(error.message)
+      }
+    }
+
+    if (!ingnore) fetchPopulation()
+
+    return () => {
+      ingnore = true
+    }
+  }, [checkedPrefectures])
+
+  return prefecuturePopulations
+}
