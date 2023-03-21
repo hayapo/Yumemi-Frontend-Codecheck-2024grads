a55475f71f3a63413aba928320ba28f25639dff6
hayapo <keep.forward1001@gmail.com>
2023-03-22 03:16:32 +0900

    [chore] PrefectureCheckBoxフォルダを作成

--- src/features/PrefectureCheckBox/__tests__/PrefectureCheckBox.test.tsx
+++ src/features/PrefectureCheckBox/__tests__/PrefectureCheckBox.test.tsx
@@ -3,7 +3,7 @@ import "@testing-library/jest-dom"
 import { render, screen } from "@testing-library/react"
 import { CheckBox } from "@/components"
 import { mockPrefecturesData } from "@/mocks/mockPrefecturesData"
-import { default as PrefectureCheckBoxSection } from "../components/PrefectureCheckBox"
+import { default as PrefectureCheckBoxSection } from "../components/PrefectureCheckBox/PrefectureCheckBox"
 import { PrefecturesWithCheckedProvider } from "@/contexts"
 
 test("should render <CheckBox ./> correctly", () => {
