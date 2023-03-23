895f8740397879a043ce0971a8612d9147d217a3
hayapo <keep.forward1001@gmail.com>
2023-03-24 04:03:08 +0900

    [chore] mode=analyzeのときだけバンドルサイズを可視化するようにした

--- vite.config.ts
+++ vite.config.ts
@@ -4,37 +4,40 @@ import { visualizer } from "rollup-plugin-visualizer"
 import gzipPlugin from "rollup-plugin-gzip"
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    assetsInlineLimit: 20480,
-    cssCodeSplit: true,
-    cssTarget: "chrome110",
-    minify: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-dom"],
+export default defineConfig(({ mode }) => {
+  return {
+    build: {
+      assetsInlineLimit: 20480,
+      cssCodeSplit: true,
+      cssTarget: "chrome110",
+      minify: true,
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vendor: ["react", "react-dom"],
+          },
+          experimentalMinChunkSize: 40960,
         },
-        experimentalMinChunkSize: 40960,
+      },
+      sourcemap: true,
+      target: "chrome110",
+    },
+    plugins: [
+      react(),
+      mode === "analyze" &&
+        visualizer({
+          open: true,
+          filename: "dist/stats.html",
+          gzipSize: true,
+          brotliSize: true,
+        }),
+      splitVendorChunkPlugin(),
+      gzipPlugin({ fileName: ".gz" }),
+    ],
+    resolve: {
+      alias: {
+        "@/": `${__dirname}/src/`,
       },
     },
-    sourcemap: true,
-    target: "chrome110",
-  },
-  plugins: [
-    react(),
-    visualizer({
-      open: true,
-      filename: "dist/stats.html",
-      gzipSize: true,
-      brotliSize: true,
-    }),
-    splitVendorChunkPlugin(),
-    gzipPlugin({ fileName: ".gz" }),
-  ],
-  resolve: {
-    alias: {
-      "@/": `${__dirname}/src/`,
-    },
-  },
+  }
 })
